be107cd8c5c353413eda66b1c37bde22894120fd
Gautham Prabhu M <71874814+GauthamPrabhuM@users.noreply.github.com>
2021-03-22 12:20:36 +0530

    Create petiaandstrings.cpp

---
+++ petiaandstrings.cpp
@@ -0,0 +1,19 @@
+#include<strings.h>
+#include<iostream>
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    char A[100];
+    char B[100];
+    gets(A);
+    gets(B);
+    cout<<strcmpi(A,B);
+    
+    
+   return 0; 
+    
+}
