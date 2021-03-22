d249783ffbeb6e23ae4fe3a4d2a64026b2935784
Gautham Prabhu M <71874814+GauthamPrabhuM@users.noreply.github.com>
2021-03-22 11:35:31 +0530

    Create limarckandbear.cpp

---
+++ limarckandbear.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    
+    int a;
+    int b;
+    int years=0;
+    cin>>a>>b;
+    while(a<=b)
+    {
+        years++;
+        a*=3;
+        b*=2;
+        
+        
+        
+        
+    }
+    
+    cout<<years;
+    
+    
+    
+    
+    return 0;
+}
