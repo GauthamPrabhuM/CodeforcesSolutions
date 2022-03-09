e082250364fe08d9b53a28b550528e0aa2298f81
Gautham Prabhu M <71874814+GauthamPrabhuM@users.noreply.github.com>
2022-03-09 10:40:12 +0530

    Create divplusmod.cpp

---
+++ divplusmod.cpp
@@ -0,0 +1,76 @@
+    #include <iostream>
+    #include<bits/stdc++.h>
+    #include<stdio.h> 
+    #include<string.h>
+    #include<ext/pb_ds/assoc_container.hpp>
+    #include<ext/pb_ds/tree_policy.hpp>
+   #define pb push_back
+   #define cl CLOCKS_PER_SEC
+   #define pi acos(-1.0)
+   #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+   #define flw freopen ("/Desktop/misc/output.txt","w",stdout);
+   #define flr freopen ("/Desktop/misc/input.txt","r",stdin);
+   #define fl freopen ("output.txt","w",stdout);freopen ("input.txt","r",stdin)
+  #define pii pair<ll,ll>
+  #define FOR(i,a,b) for(ll (i)=(a); i<(b); i++)
+  #define ROF(i,a,b) for(ll (i)=(a); i>=(b); i--)
+#define MOD 1000000007
+  #define F first
+  #define S second
+  #define all(x) (x).begin(), (x).end()
+  #define PB push_back
+  #define MP make_pair
+  typedef long long ll;
+     using namespace std;
+ll ceil2(ll a, ll b) {
+    if (a == 0) return 0;
+    return (a - 1)/b + 1;
+}
+
+    void solve()
+    {  
+        ll l;
+        ll r;
+        ll a;
+        cin>>l>>r>>a;
+        ll a1=l/a;
+        ll b1=l%a;
+        ll a2=r/a;
+        ll b2=r%a;
+        if(a1==a2)
+            cout<<a2+b2<<endl;
+        else
+            cout<<max(a2+b2,a2+a-2)<<endl;
+
+
+    }
+       
+
+
+    
+        
+    
+    
+ int main()
+    {
+        ios::sync_with_stdio(0);
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        cout<<fixed;
+        cout<<setprecision(10);
+        #ifndef ONLINE_JUDGE
+        fl;
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";   
+    #endif
+       
+      ll t=1;
+     cin>>t;
+        //sieve();
+        // pre();
+        for(int i=1;i<=t;i++)
+                    
+           solve();
+      
+        return 0;
+    }
